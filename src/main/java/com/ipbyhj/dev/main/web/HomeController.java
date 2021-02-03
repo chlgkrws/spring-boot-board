8435ec4972ffd7bff7d6a79b727b3c803357519b
LAPTOP-5J8Q1ICG\cgw981 <cgw981@naver.com>
2021-02-04 08:42:28 +0900

    Create Board View

--- src/main/java/com/ipbyhj/dev/main/web/HomeController.java
+++ src/main/java/com/ipbyhj/dev/main/web/HomeController.java
@@ -1,6 +1,7 @@
 package com.ipbyhj.dev.main.web;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,21 @@ public class HomeController {
 
 		modelAndView.addObject("boldType", "home");
 		modelAndView.setViewName("dev/main/index");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	public ModelAndView getSearchResult(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response) {
+
+		/**
+		 * 검색 키워드 코드 값으로 url 반환
+		 */
+		
+		/**
+		 * 검색 총 결과 내용(제목, 내용) 반환
+		 */
+		
+		modelAndView.setViewName("dev/search/result");
 		return modelAndView;
 	}
 }
