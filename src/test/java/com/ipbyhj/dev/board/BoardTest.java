ea1686eff962d95ba84207c9e10cc902065ff35e
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-04-12 14:54:08 +0900

    게시물 조회 테스트 코드

--- src/test/java/com/ipbyhj/dev/board/BoardTest.java
+++ src/test/java/com/ipbyhj/dev/board/BoardTest.java
@@ -1,5 +1,39 @@
 package com.ipbyhj.dev.board;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class BoardTest {
 
+	@Autowired
+    MockMvc mockMvc;
+
+	/**
+	 * 게시물 조회(성공 케이스)
+	 * boardId : 1,
+	 * @throws Exception
+	 */
+	 @Test
+	 public void selectBoard() throws Exception {
+
+	    mockMvc.perform(get("/board/101"))
+	            .andExpect(status().isOk())
+	            .andDo(print());
+	}
 }
