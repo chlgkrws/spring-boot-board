2cb0b111a473287b85d5be0246e32d0dd26356e8
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-04-14 23:38:44 +0900

    build : Add @EnableJpaRepositories in ApplicationClass

--- src/main/java/com/ipbyhj/dev/IntegratedProjectApplication.java
+++ src/main/java/com/ipbyhj/dev/IntegratedProjectApplication.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * AOP 사용 애노테이션 : @EnableAspectJAutoProxy @EnableConfigurationProperties
+ * JPA 사용 애노테이션 : @EnableJpaRepositories
  * @author cgw981
  *
  */
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties
+@EnableJpaRepositories
 @SpringBootApplication
 public class IntegratedProjectApplication {
 
