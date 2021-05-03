a564926b2cdc14551b9dc9f45a817c8ccbbdbbe4
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-05-03 23:02:34 +0900

    chore : Change StringUtils lang -> lang 3

--- src/main/java/com/ipbyhj/dev/board/service/ReplyService.java
+++ src/main/java/com/ipbyhj/dev/board/service/ReplyService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.BiPredicate;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
