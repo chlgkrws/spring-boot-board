3c002bc4bd09aea73717480a4777dc3b0b4d8546
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-06-18 17:48:15 +0900

    feat : Add init method in BaseEntity

--- src/main/java/com/ipbyhj/dev/common/entity/BaseEntity.java
+++ src/main/java/com/ipbyhj/dev/common/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.ipbyhj.dev.common.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 import lombok.Getter;
@@ -14,7 +15,20 @@ public abstract class BaseEntity {
 
 	private LocalDateTime createTime;
 	private LocalDateTime updateTime;
+
+	@Column(updatable = false)
 	private String createBy;
 	private String updateBy;
 
+
+	public void initDefaultValues() {
+		this.createTime = LocalDateTime.now();
+		this.updateTime = LocalDateTime.now();
+	}
+
+	public void BeforefirstInsert(String createBy) {
+		this.createBy = createBy;
+		this.updateBy = createBy;
+		initDefaultValues();
+	}
 }
